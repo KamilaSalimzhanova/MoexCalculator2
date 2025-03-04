8c78c4a519a116f3a9b2430967196a66f381debf
kamila <ksalimzh@gmail.com>
2025-03-05 00:33:14 +0500

    Currency file declaration is added (enum and struct with type alias)

--- MoexCalculator/ContentView.swift
+++ MoexCalculator/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var color = Color.green
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Привет, Практикум!")
+                .padding()
+                .foregroundColor(color)      // 2
+            Button("Нажми здесь!") {         // 3
+                if color == .green {
+                    color = .orange
+                } else {
+                    color = .green
+                }
+            }
         }
-        .padding()
     }
 }
 
---
+++ MoexCalculator/Currency.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+enum Currency: String, Identifiable, CaseIterable {
+    case RUR
+    case CNY
+    case EUR
+    case USD
+    
+    var id: Self { self } // for protocol identifiable
+    
+    var flag: String {
+        switch self {
+        case .RUR: return "🇷🇺"
+        case .CNY: return "🇨🇳"
+        case .EUR: return "🇪🇺"
+        case .USD: return "🇺🇸"
+        }
+    }
+        
+}
+
+typealias currencyRates = [Currency: Double]
+
+struct CurrencyAmount {
+    let currency: Currency
+    let amount: Double
+}

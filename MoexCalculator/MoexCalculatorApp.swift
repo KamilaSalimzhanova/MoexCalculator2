1d8672830c13c26f4d3637e36ba6b39d5b0bb919
kamila <ksalimzh@gmail.com>
2025-03-05 00:15:07 +0500

    Initial Commit

---
+++ MoexCalculator/MoexCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoexCalculatorApp.swift
+//  MoexCalculator
+//
+//  Created by kamila on 05.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct MoexCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
